d0d4a24e3a3c7f8196b9111feb066e0295339738
Filipe <filipe.souza1000@gmail.com>
2025-02-15 09:33:32 -0300

    feat: adicionado controle de produtos e finalizado cadastro

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,4 +1,15 @@
 package Main;
 
+import Produto.Eletronico;
+import controle.ControleProdutos;
+
 public class Main {
+    public static void main(String[] args) {
+        ControleProdutos controleProdutos = new ControleProdutos();
+
+        Eletronico eletronico = new Eletronico(1, "Smarthphone", 3500.00, 0.3);
+
+        controleProdutos.adicionarProduto(eletronico);
+
+    }
 }
--- src/Produto/Eletronico.java
+++ src/Produto/Eletronico.java
@@ -4,9 +4,8 @@ public class Eletronico extends Produto{
 
     private double peso;
 
-    public Eletronico(double peso) {
-        super();
+    public Eletronico(int id, String nome, double preco, double peso) {
+        super(id, nome, preco);
         this.peso = peso;
     }
-
 }
