d7bd595267aabfd4b68e55b3409f4142a96ac2a9
Filipe <filipe.souza1000@gmail.com>
2025-02-15 09:18:34 -0300

    feat: adicionado escopo inicial dos produtos

---
+++ src/Produto/Eletronico.java
@@ -0,0 +1,12 @@
+package Produto;
+
+public class Eletronico extends Produto{
+
+    private double peso;
+
+    public Eletronico(double peso) {
+        super();
+        this.peso = peso;
+    }
+
+}
